17ae8f0af39b94aa445665311282a30cc5be6ea9
Drugov Nikita <drugov@qnium.com>
2024-04-01 00:30:43 +0500

    Added .npmignore Added build config Renamed package

--- example/src/index.ts
+++
@@ -1,8 +0,0 @@
-import ReactPDF from '@react-pdf/renderer';
-import ExampleDocument from './ExampleDocument';
-
-async function createPDF() {
-    await ReactPDF.render(ExampleDocument(), `example.pdf`);
-}
-
-createPDF();
